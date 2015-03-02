594a863ef22345f330b9284e47515b9ce07a2dac
istorey <wrightstoreys@gmail.com>
2015-03-02 18:54:14 -0500

    edited hero icon

---
+++ javascript/game.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  setUpGame()
+  updateCanvas() //Loop
+
+  ////Functions run on load
+  function setUpGame() {
+    createKeyListenerController()
+    addEnemyInterval() //new enemy added every 2 seconds
+    $("#game").html(Game.canvas)
+  }
+
+  function updateCanvas() {
+    //Guard clause to end updateCanvas Loop
+    if (Game.over) {
+      return
+    }
